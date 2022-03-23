b1f2cb2d40d2ed68c24c6d909da4381d57be9da2
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-23 11:49:45 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          colorScheme: const ColorScheme.light(
+              primary: Colors.teal,
+              primaryContainer: Colors.tealAccent,
+              secondary: Colors.purple,
+              secondaryContainer: Colors.purpleAccent)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text("Hello World"),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
